452093a1a967b454c30bf5316f4aa0f28fdbf80f
Adamari R <adasanrodriguez@gmail.com>
2026-08-26 10:57:36 -0400

    setting up the init still

--- src/new-init.js
+++ src/new-init.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import { Parser } from './input-processing/Parser';
+import { Processor } from './input-processing/Processor';
 
 /* 
     gridhelper example. consider orbit ctrls? 
@@ -34,6 +36,9 @@ function animate() {
 animate()
 */
 
+/* vFlds container for callbacks */
+const vFlds = []
+
 /* axes and grid */
 const axes = new THREE.AxesHelper(10)
 const grid = new THREE.GridHelper(10, 10, 0x9bdc6e, 0x333333)
@@ -57,16 +62,47 @@ genFolder.add(params, 't', -50, 50, 0.1)
     gui.addColor(component.material, 'color')
 */
 
-const components = []
+/* callback when vfld slices changed */
+const VectorFieldCallback = (newslices, vfld) => {
+    /* clear old slices from display */
+    vfld.group.children = []
+    let i = 0 
+    let j = 0
+    let k = 0
+    for (let x = -newslices[0]; x < newslices[0]; x++) {
+        for (let y = -newslices[1]; y < newslices[1]; y++) {
+            for (let z = -newslices[2]; z < newslices[2]; z++) {
+                try {
+                    vfld.group.add(vfld.slices[i][j][k])
+                } catch (e) {
+                    console.error(`${e}`)
+                }
+                k+=1
+            }
+            k = 0
+            j+=1
+        }
+        j = 0
+        i+=1
+    }
+}
 
-components.map((component) => {
+Processor(Parser()).map((component) => {
     let newFolder = gui.addFolder(component.name)
     // vfld uses a gradient 
     // hmm... so does function. maybe find a better way
-    if (!(component.name.includes('VFld'))) {
-        newFolder.addColor(component.mesh.material, 'color')
+    if (component.name.includes('VFld')) {
+        console.log('todo vfld, slices and colors')
+    } else {
+        newFolder.add(component, 'xMax', 5, 50, 1)
+        newFolder.add(component, 'yMax', 5, 50, 1)
+        newFolder.add(component, 'zMax', 5, 50, 1)
+        if (component.name.includes('Func')) {
+            console.log('todo func, gradient colors')
+        } else if (component.name.includes('Vec')) {
+            console.log('todo vec, line and cone colors')
+        } else {
+            newFolder.addColor(component.mesh.material, 'color')
+        }
     }
-    newFolder.add(component, 'xMax', 5, 50, 1)
-    newFolder.add(component, 'yMax', 5, 50, 1)
-    newFolder.add(component, 'zMax', 5, 50, 1)
 })
